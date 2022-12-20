a6fa7844195fb7e1b6e092f848e05fce46ac14b1
Darko Trifkovic <darko_trifkovic@epam.com>
2022-12-20 13:08:49 +0100

    counters done

--- src/components/class-based/SimpleCounterClass.js
+++ src/components/class-based/SimpleCounterClass.js
@@ -3,13 +3,24 @@ import React, { Component } from "react";
 
 export default class SimpleCounterClass extends Component {
   // napraviti state koji se menja pritiskom na klik buttona 
+
+  state = {
+    count: 0
+  }
+
+  incrementCount = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+
   render() {
     return (
       <div>
         <h3>Counter class-based</h3>
 
-        <p>0</p>
-        <button>Add 1</button>
+        <p>{this.state.count}</p>
+        <button onClick={this.incrementCount}>Add 1</button>
       </div>
     );
   }
--- src/components/function-based/SimpleCounterFunction.js
+++ src/components/function-based/SimpleCounterFunction.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SimpleCounterFunction = () => {
   // napraviti state koji se menja pritiskom na klik buttona preko useState hook-a
+  const [count, setCount] = useState(0);
+
+  function incrementCount() {
+    setCount(prevCount => prevCount + 1);
+  }
+
   return (
     <div>
       <h3>Counter function-based</h3>
-      <p>0</p>
-      <button>Add 1</button>
+      <p>{count}</p>
+      <button onClick={incrementCount}>Add 1</button>
     </div>
   );
 };
